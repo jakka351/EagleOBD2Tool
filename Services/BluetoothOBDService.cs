4d7463075fcc2edc51d0515eb750594aff39656c
Jakka351 <jakka351@outlook.com>
2025-07-20 13:46:08 +1000

    Add files via upload

---
+++ Services/BluetoothOBDService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eagle.Services
+{
+    public interface IBluetoothOBDService
+    {
+        void Connect(string portName, int baudRate = 9600);
+        void Disconnect();
+        string SendCommand(string command);
+        bool IsConnected { get; }
+    }
+}
